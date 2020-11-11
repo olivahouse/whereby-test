7c11f09f0b738fcac6d2a2625f9c0fedbfc5a953
Marc <mwgdavies@gmail.com>
2020-11-11 15:04:12 +0100

    Start project

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+         👋  Hi Whereby, I'm going to test you here.
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
